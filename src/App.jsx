31607406bdbe4f1a6e6e6d5c5a19f33941895b82
Vahe Sevachyan <vscode13@gmail.com>
2023-11-13 20:00:13 -0800

    updated app.js to iterate over data object and display data

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import CoreConcepts from "./CoreConcepts";
 import Header from "./Header";
-import reactConceptsImg from "./assets/components.png";
+
 import { CORE_CONCEPTS } from "./Data";
 function App() {
   return (
@@ -10,26 +10,16 @@ function App() {
         <section id="core-concepts">
           <h2>Time to get started!</h2>
           <ul>
-            <CoreConcepts
-              title={CORE_CONCEPTS[0].title}
-              description={CORE_CONCEPTS[0].description}
-              img={CORE_CONCEPTS[0].image}
-            />
-            <CoreConcepts
-              title={CORE_CONCEPTS[1].title}
-              description={CORE_CONCEPTS[1].description}
-              img={CORE_CONCEPTS[1].image}
-            />
-            <CoreConcepts
-              title={CORE_CONCEPTS[2].title}
-              description={CORE_CONCEPTS[2].description}
-              img={CORE_CONCEPTS[2].image}
-            />
-            <CoreConcepts
-              title={CORE_CONCEPTS[3].title}
-              description={CORE_CONCEPTS[3].description}
-              img={CORE_CONCEPTS[3].image}
-            />
+            {CORE_CONCEPTS.map((concepts) => {
+              const { image, title, description } = concepts;
+              return (
+                <CoreConcepts
+                  img={image}
+                  title={title}
+                  description={description}
+                />
+              );
+            })}
           </ul>
         </section>
       </main>
